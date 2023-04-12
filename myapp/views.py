e02bae6303bf590b6f97b3ff72e2419721cdb942
Govind R Nair <govindrnair.rev@ug.cusat.ac.in>
2023-04-12 22:58:03 +0530

    Template update

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ from .models import ToDoList, Item
 
 def index (response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, 'myapp/base.html', {"name":ls.name})
+    return render(response, 'myapp/list.html', {"ls":ls})
 
 def home(response):
-    return render(response, 'myapp/home.html', {"name":"test"})
+    return render(response, 'myapp/home.html', {})
