cf7b4e47c62937dcd329bc5c246834dec5eff859
Govind R Nair <govindrnair.rev@ug.cusat.ac.in>
2023-05-02 00:32:02 +0530

    view correction

--- myapp/views.py
+++ myapp/views.py
@@ -8,25 +8,27 @@ from .forms import CreateNewList
 def index (response, id):
     ls = ToDoList.objects.get(id=id)
     
-    if response.method == "POST":
-        print (response.POST)
+    if response.user.todolist.all():
         if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
-                else:
-                    item.complete = False
-                item.save()
-                
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("INVALID")
-                
-    return render(response, 'myapp/list.html', {"ls":ls})
-
+            if response.method == "POST":
+                print (response.POST)
+                if response.POST.get("save"):
+                    for item in ls.item_set.all():
+                        if response.POST.get("c" + str(item.id)) == "clicked":
+                            item.complete = True
+                        else:
+                            item.complete = False
+                        item.save()
+                        
+                elif response.POST.get("newItem"):
+                    txt = response.POST.get("new")
+                    if len(txt) > 2:
+                        ls.item_set.create(text=txt, complete=False)
+                    else:
+                        print("INVALID")
+                        
+            return render(response, 'myapp/list.html', {"ls":ls})
+    return render(response, 'myapp/view.html', {})
 def home(response):
     return render(response, 'myapp/home.html', {})
 
